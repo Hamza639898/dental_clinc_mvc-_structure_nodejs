97d919304b3e80389d80c0eca5eb1ee185109388
Ahmedmo73711 <axmedsyl0@gmail.com>
2025-02-06 09:04:37 +0300

    Add files via upload

---
+++ routes/authRoutes.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const router = express.Router();
+const { register, login } = require('../controllers/authController');
+const { protect } = require('../middleware/authMiddleware');
+
+
+router.post('/register', protect, register);
+router.post('/login', login);
+
+module.exports = router;
